e174132383036654f44424eba702793cd2116516
Chris Ching <chris.e.ching@gmail.com>
2011-03-04 18:16:10 -0700

    Updated awesome config with thumb's configs

---
+++ awesome/themes/arch/theme.lua
@@ -0,0 +1,27 @@
+---------------------------------
+-- arch awesome theme --
+---------------------------------
+ 
+font = terminus 8
+ 
+bg_normal = #000000
+bg_focus = #ffffff
+bg_urgent = #ff0000
+ 
+fg_normal = #1793d1
+fg_focus = #1793d1
+fg_urgent = #ffffff
+ 
+border_width = 1
+border_normal = #ffffff
+border_focus = #000000
+border_marked = #ff0000
+ 
+# You can use your own command to
+# set your wallpaper
+wallpaper_cmd = awsetbg ~/.config/awesome/themes/arch/background.jpg
+ 
+# You can add as many variables as
+# you wish and access them by using
+# beautiful.variable in your rc.lua
+#bg_widget = #cc0000
