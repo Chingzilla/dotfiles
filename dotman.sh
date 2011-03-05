4e3f9f5023a3e82bd1a47c0e397dcf6eb20868ec
Chris Ching <coolchingster@gmail.com>
2011-03-04 17:42:16 -0700

    Working dotman.sh with backup and link

--- dotman.sh
+++ dotman.sh
@@ -5,18 +5,34 @@ CMD=$2
 
 case "$CMD" in
     link)
+        echo Creating links for $PROGRAM:
+        while read line; do
+            #declare -a FILES
+            FILES=(`echo $line | tr '>' '\n'`)
+            BK_FILE=${FILES[0]}
+            ORG_FILE=${FILES[1]}
+            #echo Creating Symbolic link for ${ORG_FILE}...
+            #echo ln -s "$PWD/$PROGRAM/$BK_FILE" "$HOME/$ORG_FILE"
+            ln -s "$PWD/$PROGRAM/$BK_FILE" "$HOME/$ORG_FILE"
+        done < ./$PROGRAM/files 
+    ;;
+
+    backup)
+        echo Backing up files for $PROGRAM:
         while read line; do
             FILES=(`echo $line | tr '>' '\n'`)
             BK_FILE=${FILES[0]}
             ORG_FILE=${FILES[1]}
-            echo Creating Symbolic link for $ORG_FILE...
-            ln -s $PWD/$PROGRAM/$BK_FILE $ORG_FILE
+            #echo Coping $ORG_FILE to $BK_FILE
+            #echo cp -r -i $ORG_FILE $PWD/$PROGRAM/$BK_FILE
+            cp -rv $HOME/$ORG_FILE $PWD/$PROGRAM/$BK_FILE
         done < ./$PROGRAM/files 
     ;;
-    backup)
-        echo "todo"
-    ;;
+
     *)
         echo "usage: $0 {link, backup}"
+    ;;
+
 esac
+
 exit 0
